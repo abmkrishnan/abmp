d889a55c077183d99ac29b5fafeca1a79754a7f7
Palanikrishnan <abmkrishnan@gmail.com>
2015-08-24 12:58:06 +0530

    Initial Commit

---
+++ sphinx4trainer/src/com/abmp/sphinx4trainer/main/Sphinx4Trainer.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.abmp.sphinx4trainer.main;
+
+/**
+ * @author Palanikrishnan
+ *
+ */
+public class Sphinx4Trainer
+{
+	/**
+	 * 
+	 */
+	public Sphinx4Trainer()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	}
+}
